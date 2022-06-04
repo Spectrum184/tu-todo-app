4508387849e466d161df4e0cc4020ab31060c129
Anh Tu Dang <anhtudang95@gmail.com>
2022-06-04 18:06:15 +0900

    add Login page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './Layout.module.css';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
-  return <main className={clsx(styles.mainLayout)}>{children}</main>;
+  const { pathname } = useRouter();
+  console.log(pathname);
+
+  return (
+    <main className={clsx(styles.mainLayout)}>
+      {children}
+      {pathname.includes('login') && <h2>tu ngu si dan</h2>}
+    </main>
+  );
 };
 
 export default Layout;
